bad792a1bb2d298e1df6522b0afaf8399d87a8ef
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2023-07-12 19:49:10 +0530

    Add files via upload
    save

---
+++ LEARNING JS FOR webdevelopment/let variable/js/scritpt.js
@@ -0,0 +1,22 @@
+let name="giri";
+
+// in this let keyword we able to change change the name or value what we given in the variable 
+
+name="giridharan";
+
+const Name="giri";
+const DOB="11-08-2001";
+
+
+
+let msg=" hello i'm " + Name + " i born on " +DOB+ " i'm learning javascript";
+
+alert(msg);
+
+console.log(msg);
+
+// constant is non-chanagble
+
+
+
+
