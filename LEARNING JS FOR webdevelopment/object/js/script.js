7941e01b138a0fe6aeadeddf998012051059251d
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2023-07-12 20:56:26 +0530

    Add files via upload
    save

---
+++ LEARNING JS FOR webdevelopment/object/js/script.js
@@ -0,0 +1,19 @@
+let firstname="giri";
+let lastname="dharan";
+let place="devakottai";
+
+let Name="hi i am " + firstname+ "" +lastname+ " i am from " + place+ "";
+
+alert(Name);
+
+let person = {
+    name:"hari",
+    age:24,
+    gender:"male",
+    sibling:{
+        brother:"giri",
+        sister:"sathya"
+    }
+}
+
+console.log(person.sibling.sister)
