59e4e35ec48d5503b6c11fc6b9c549dfa51826af
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2023-07-12 19:14:59 +0530

    Add files via upload
    SAVE

---
+++ LEARNING JS FOR webdevelopment/variable/js/script.js
@@ -0,0 +1,14 @@
+let name="giri";
+let msg=" hello " + name + " where are you";
+
+alert(msg);
+
+console.log(msg);
+
+let number1=5;
+let number2=45;
+let result=number1+number2;
+
+alert(result);
+
+console.log(result);
