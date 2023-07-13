17b85ab7c160f4265330d96ace1dee98bf7bec93
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2023-07-14 04:27:26 +0530

    Add files via upload
    save

---
+++ LEARNING JS FOR webdevelopment/conditional statement/switch case/js/script.js
@@ -0,0 +1,33 @@
+let grade ="A";
+
+switch(grade){
+    case "O":
+        console.log("super,TOPPER");
+        break;
+
+    case "A":
+        console.log("best, second"); 
+        break;
+
+    case "B":
+        console.log("Keep learn");
+        break;
+
+    case "C":
+        console.log("FAIL");
+       break;
+
+    default:
+        console.log("non of the above");
+}
+
+let name="giri";
+
+switch(name){
+    case "giri":
+        console.log("correct");
+        break;
+    default:
+        console.log("wrong")
+
+}
