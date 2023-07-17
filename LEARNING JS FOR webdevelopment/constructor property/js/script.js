6869f105a1d382f43c84d26cb691ef7ba646b9f5
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2023-07-17 19:44:13 +0530

    Add files via upload
    save

---
+++ LEARNING JS FOR webdevelopment/constructor property/js/script.js
@@ -0,0 +1,23 @@
+let person={
+    name:"giri",
+    age:22,
+
+
+
+}
+
+console.log(constructor);
+
+// let name="giri"; //--> new string("giri")
+
+// let age=22;//--> new number(22);
+
+// console.log(name,age);
+
+// constructor property
+
+let name=  String("giri");
+
+let age =  Number(22);
+
+console.log(name,age);
